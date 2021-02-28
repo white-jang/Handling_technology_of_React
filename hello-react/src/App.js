c47f8a67eff9be1745b51e6e991f6cae57da416b
white-jang <galaxy__12@naver.com>
2021-02-28 21:23:38 +0900

    ~4chap

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 function App() {
-  return <Say></Say>;
+  return <EventPractice></EventPractice>;
 }
 
 export default App;
---
+++ hello-react/src/EventPractice.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const EventPractice = () => {
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // spread 연산자로 기존 객체 복사
+      [e.target.name]: e.target.value, // 원하는 값 덮어씌우기 (update)
+    };
+    setForm(nextForm);
+  };
+  const onClick = () => {
+    alert(username + ": " + message);
+    setForm({ username: "", message: "" });
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        value={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+
+export default EventPractice;
