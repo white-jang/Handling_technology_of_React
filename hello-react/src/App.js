47ccca0d5bb5083420fffb0a369ce7cbc636a80f
white-jang <galaxy__12@naver.com>
2021-03-06 18:01:54 +0900

    ~6chap

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
-import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
