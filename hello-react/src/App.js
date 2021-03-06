b327c08be3f7ce0510f2dacac6603564238a5695
white-jang <galaxy__12@naver.com>
2021-03-06 17:06:25 +0900

    ~5chap

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-import EventPractice from "./EventPractice";
+import React, { Component } from "react";
+import ScrollBox from "./ScrollBox";
 
-function App() {
-  return <EventPractice></EventPractice>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
