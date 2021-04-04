d9dd0f128b77288c064284fb8103d3ec1ad74a7e
white-jang <galaxy__12@naver.com>
2021-04-04 23:07:08 +0900

    ~8chap

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,33 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
+import useInputs from "./useInputs";
 
 const Info = () => {
-  // useState는 가장 기본적인 Hooks로 함수형 컴포넌트에서도 상태 관리를 할 수 있게 해준다.
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  useEffect(() => {
-    // useEffect는 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hooks.
-    // class형 컴포넌트의 componentDidMount, componentDidUpdate를 합친 형태로 봐도 무방.
-    // console.log("렌더링이 완료되었습니다!");
-    console.log("effect");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  }, [name]);
+  const [state, dispatch] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  const onChange = (e) => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
