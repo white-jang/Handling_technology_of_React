c2ecc4f3c27d227a9dd531589d2f695fa4dd5dcb
white-jang <galaxy__12@naver.com>
2021-04-17 00:11:48 +0900

    14chap - 라우터를 활용

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,31 +1,9 @@
-import React, { useState, useCallback } from 'react';
-// import axios from 'axios';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  // const [data, setData] = useState(null);
-
-  // const onClick = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       'https://newsapi.org/v2/top-headlines?country=kr&apiKey=36e5d513009143b895daee4deb045211',
-  //     );
-  //     setData(response.data);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
-
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,25 +18,11 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // async를 내부에서 사용하고 싶다면 async가 붙은 또다른 함수를 만들어서 사용해야 함
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=36e5d513009143b895daee4deb045211`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=36e5d513009143b895daee4deb045211`,
+    );
   }, [category]);
 
   // 로딩(대기) 중일 때
@@ -43,11 +30,18 @@ const NewsList = ({ category }) => {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
 
-  // 아직 articles 값이 설정되지 않았을 때
-  if (!articles) {
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
     return null;
   }
-  // articles 값이 유효할 때
+
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
