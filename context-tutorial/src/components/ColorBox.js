57b9dd3d5ccc9093bf8da30dfe985f315a4219fa
white-jang <galaxy__12@naver.com>
2021-05-11 13:58:58 +0900

    useContext Hook 사용

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,34 +1,55 @@
-import React from "react";
-import { ColorConsumer } from "../contexts/color";
+import React, { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
-  return (
-    // Consumer를 이용해 Context 값 조회
-    // or useContext Hook을 이용해 Context 값 조회
-    <ColorConsumer>
-      {/* Consumer 사이 (children 자리)에 JSX나 문자열이 아닌 함수를 넣음 
-      => Function as a child, Render Props 패턴 */}
-      {(value) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: value.state.color,
-            }}
-          />
+  // useContext Hook을 활용하면 간편하게 조회
+  const { state } = useContext(ColorContext);
 
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: value.state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+  return (
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
 export default ColorBox;
+
+// Consumer를 이용해 Context 값 조회
+// or useContext Hook을 이용해 Context 값 조회
+// <ColorConsumer>
+//   Consumer 사이 (children 자리)에 JSX나 문자열이 아닌 함수를 넣음
+//   => Function as a child, Render Props 패턴
+//   {(value) => (
+//     <>
+//       <div
+//         style={{
+//           width: "64px",
+//           height: "64px",
+//           background: value.state.color,
+//         }}
+//       />
+
+//       <div
+//         style={{
+//           width: "32px",
+//           height: "32px",
+//           background: value.state.subcolor,
+//         }}
+//       />
+//     </>
+//   )}
+// </ColorConsumer>
