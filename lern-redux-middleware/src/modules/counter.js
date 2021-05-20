ff226bf7e0f4f60763e800fd7faf3751f71427b2
white-jang <galaxy__12@naver.com>
2021-05-20 23:48:42 +0900

    18chap - Redux Saga

--- lern-redux-middleware/src/modules/counter.js
+++ lern-redux-middleware/src/modules/counter.js
@@ -1,24 +1,42 @@
 import { createAction, handleActions } from "redux-actions";
+import { delay, put, takeEvery, takeLatest, select } from "redux-saga/effects";
 
 const INCREASE = "conter/INCREASE";
 const DECREASE = "conter/DECREASE";
 
+const INCREASE_ASYNC = "conter/INCREASE_ASYNC";
+const DECREASE_ASYNC = "conter/DECREASE_ASYNC";
+
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-const initialState = 0; // 초기 상태 (객체가 아니어도 됨)
+// 마우스 클릭 이벤트가 payload 안에 들어가지 않도록
+// () => undefined를 두 번째 파라미터로 넣어 줍니다
 
-// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
+export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
+export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
+
+function* increaseSaga() {
+  yield delay(1000); // 1초 wait
+  yield put(increase()); // 특정 액션 디스패치
+  const number = yield select((state) => state.counter); // state는 스토어 상태
+  console.log(`현재 값은 ${number}입니다`);
+}
+
+function* decreaseSaga() {
+  yield delay(1000); // 1초 wait
+  yield put(decrease()); // 특정 액션 디스패치
+}
+
+export function* counterSaga() {
+  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리
+  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고
+  // 가장 마지막으로 실행된 작업만 수행합니다
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
+
+const initialState = 0;
 
 const counter = handleActions(
   {
--- lern-redux-middleware/src/modules/index.js
+++ lern-redux-middleware/src/modules/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from "redux";
-import counter from "./counter";
-import sample from "./sample";
+import { all } from "redux-saga/effects";
+import counter, { counterSaga } from "./counter";
+import sample, { sampleSaga } from "./sample";
 import loading from "./loading";
 
 const rootReducer = combineReducers({ counter, sample, loading });
+
+export function* rootSaga() {
+  // all 함수는 여러 사가를 합쳐주는 역할
+  yield all([counterSaga(), sampleSaga()]);
+}
 
 export default rootReducer;
