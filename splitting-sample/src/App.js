aa3cf0822b19e5d7af451778c7cf100cda98005d
white-jang <galaxy__12@naver.com>
2021-07-02 22:26:54 +0900

    ~19chap - lazy & Suspense

---
+++ splitting-sample/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, Suspense } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const SplitMe = React.lazy(() => import("./SplitMe"));
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello, React!</p>
+        <Suspense fallback={<div>Loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ splitting-sample/src/SplitMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SplitMe = () => {
+  return <div>Split Me!</div>;
+};
+
+export default SplitMe;
